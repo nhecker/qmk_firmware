feba0b4436c150117e833cfb1efd0b13e2e102bd
Nick Hecker <nicholas.hecker@hyland.com>
2026-05-07 23:05:47 -0400

    fix: slow your roll

--- keyboards/keebio/dilly/keymaps/nhecker/keymap.c
+++ keyboards/keebio/dilly/keymaps/nhecker/keymap.c
@@ -73,7 +73,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 static uint8_t rosc_random_byte(void) {
   uint8_t byte = 0;
   for (int i = 0; i < 8; ) {
+    wait_us(10);
     uint8_t a = rosc_hw->randombit & 1;
+    wait_us(10);
     uint8_t b = rosc_hw->randombit & 1;
     if (a != b) {
       byte = (byte << 1) | a;
